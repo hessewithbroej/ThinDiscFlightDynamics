df2f603a6b5fbaa3599003494e430bf2746de9d0
Joe Hesse-Withbroe <hessewithbroej@hotmail.com>
2023-11-03 13:04:46 -0600

    regenerated trajectory plots and data for full range of phi (0,360)

--- Matlab - Plotting Routines/trajectory_plots.m
+++ Matlab - Plotting Routines/trajectory_plots.m
@@ -2,7 +2,7 @@
 %coordinates over time. Also have access to phi and alpha over time, if one
 %wanted to color trajectories as a function of phi to demonstrate how the
 %range-maximizing orientation extracts lift.
-filepath = "C:\Users\hesse\Desktop\Academic Documents\Seeds\Data\SeedTrajectories.xlsx";
+filepath = "C:\Users\hesse\Desktop\Code\ThinDiscFlightDynamics\Data\output1000.xlsx";
 
 %read in the data
 trajs = {};
@@ -12,36 +12,71 @@ for i=2:numel(shts)
   traj = readmatrix(filepath,'Sheet',shts(i),'Range','A2:F1002');
   trajs{i-1} = traj;
 end
+%% 
+% %color triplets for rainbow colormap
+% rainbow = [1,        0,             0;
+%            1.0000    0.3333         0;
+%            1.0000    0.6667         0;
+%            1         1              0;
+%            0.6667    1.0000         0;
+%            0.3333    1.0000         0;
+%            0         1.0000         0;
+%            0         1.0000         0.3333;
+%            0         1.0000         0.6667;
+%            0         1.0000         1;
+%            0         0.6667         1;
+%            0         0.3333         1;
+%            0         0              1;
+%            0.3333    0              1;
+%            0.6667    0              1;
+%            1         0              1;
+%            1         0              0.6667;
+%            1         0              0.3333;
+%            0         0              0];
 
-%color triplets for rainbow colormap
-rainbow = [1,        0,             0;
-           1.0000    0.3333         0;
-           1.0000    0.6667         0;
-           1         1              0;
-           0.6667    1.0000         0;
-           0.3333    1.0000         0;
-           0         1.0000         0;
-           0         1.0000         0.3333;
-           0         1.0000         0.6667;
-           0         1.0000         1;
-           0         0.6667         1;
-           0         0.3333         1;
-           0         0              1;
-           0.3333    0              1;
-           0.6667    0              1;
-           1         0              1;
-           1         0              0.6667;
-           1         0              0.3333;
-           0         0              0];
 
+rainbow = [];
+for i =0:35
+
+    if i<6
+        r = i*(1/6);
+        g = 0;
+        b = 0;
+    elseif i < 12
+        r = 1;
+        g = mod(i,6)*(1/6);
+        b = 0;
+
+    elseif i < 18
+        r = 1-mod(i,6)*(1/6);
+        g = 1;
+        b = 0;
+
+    elseif i < 24
+        r = 0;
+        g = 1;
+        b = mod(i,6)*(1/6);
+
+    elseif i < 30
+        r = 0;
+        g = 1-mod(i,6)*(1/6);
+        b = 1;
+    else
+        r = 0;
+        g = 0;
+        b = 1-mod(i,6)*(1/6);
+    end
+
+    rainbow(end+1,:) = [r g b];
+end
 
 figure
 hold on
 %plot the trajectory from launch until the first instance where the seed's
 %y coordinate goes negative (aka seed has reached the ground)
 for i =1:numel(trajs)
-    gndrow = find(trajs{i}(:,3) <= 0, 1); %find when seed hits ground
-    plot3(trajs{i}(1:gndrow,2), trajs{i}(1:gndrow,4), trajs{i}(1:gndrow,3), 'Color',rainbow(i,:), 'LineWidth', 1.5)
+    gndrow = find(trajs{i}(:,4) <= 0, 1); %find when seed hits ground
+    plot3(trajs{i}(1:gndrow,3), trajs{i}(1:gndrow,5), trajs{i}(1:gndrow,4), 'Color',rainbow(i,:), 'LineWidth', 1.5)
     
 end
 grid on
